1e091bffff4282be7e09d7e09e56dec0259bb9f1
Alex Waygood <alex.waygood@gmail.com>
2021-06-03 19:33:32 +0100

    attempt to add a link preview #7

--- main.py
+++ main.py
@@ -4,29 +4,49 @@ use_case.WEB_MODE = True
 
 from typing import Optional, Tuple
 from warnings import filterwarnings
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from src.web_app.web_graph_plotting import WebGraphPlotter
 
 
 filterwarnings('ignore')
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 plotter: Optional[WebGraphPlotter] = None
+
+
+### Configuration routes ###
 
 
 @app.before_first_request
 def InitialiseManager() -> None:
+    """
+    Initialises an o plotter object that will grab the FT's data from Github
+    in the background and start wrangling it
+    """
+
     global plotter
     plotter = WebGraphPlotter()
+
+
+@app.route('/robots.txt')
+def static_from_root() -> str:
+    """The robots.txt file defines whether robots are allowed to crawl this site or not."""
+
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
+@app.route('/link-image/')
+def preview() -> str:
+    """The image that's shown on links when they're shared on Facebook/Twitter"""
+
+    return send_from_directory(app.static_folder, 'link-graphic.png')
+
+
+### Specific webpage routes ###
 
 
 @app.route('/')
 def home() -> str:
     return render_template('home.html', plotter=plotter.Reset(), AboutPage=False)
-
-
-@app.route('/link-preview.png')
-def preview() -> str:
-    return send_from_directory('static', 'images/covid.jpg')
 
 
 @app.route('/about/')
@@ -39,6 +59,9 @@ def dataviewer(FromRedirect=False) -> str:
     if plotter.Update(FromRedirect).IncorrectEntry:
         return dataviewer(FromRedirect=True)
     return render_template('data_viewer.html', plotter=plotter, FromRedirect=FromRedirect, AboutPage=False)
+
+
+### Error handlers ###
 
 
 # noinspection PyUnusedLocal
